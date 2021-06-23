0cf6393f320b2ce4448ce30a5454b8cc67f9bb6f
david-1-nguyen <40614920+david-1-nguyen@users.noreply.github.com>
2021-06-22 19:57:31 -0700

    Removed random comments

--- graphql/resolvers/sales.js
+++ graphql/resolvers/sales.js
@@ -2,28 +2,12 @@ const Sales = require('../models/Sale')
 const User = require('../models/User')
 const checkAuth = require('../../utils/checkAuth')
 
-/* Special Datatype: Service
-* services are offered salon services such as waxing manicures pedicures etc...
-*
-    title: String => specific service of specified category
-    price: Float
-    description: String
-    category: String => general category such as waxing/manicure/pedicure
-    date: String => when this service was added
-    user (creator): who created this service (admin only access)
-* */
 
 module.exports = {
     Query: {
 
     },
     Mutation: {
-        /*
-        * adds a Service object to mongodb
-        * title(string), price(float), descr(string), category(String), context(browser header) -> service added(Service)
-        * features: checks context if authorization token provided and user privileges provided,
-        * checks if input service title is already added,
-        * */
         async addSales(_, {
             saleInput: {
                 customerName,
